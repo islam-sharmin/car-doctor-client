4ab1cde156da225e6fd20a42abd3ed7e8c2eb2cc
Sharmin2622 <islam.sharmin111@gmail.com>
2024-05-06 14:06:56 +0600

    car-doctor navbar banner about services section added

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer";
+import NavBar from "../pages/Shared/NavBar/NavBar";
+
+
+const Main = () => {
+    return (
+        <div>
+            <div className="max-w-6xl mx-auto">
+                <NavBar></NavBar>
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
